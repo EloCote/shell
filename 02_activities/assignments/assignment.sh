ff4301440f948337cf3c3d01805f72fc4bedd328
EloCote <eloise.cote@mail.utoronto.ca>
2026-03-06 19:00:22 -0500

    question 1

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -31,7 +31,7 @@ unzip -q rawdata.zip
 mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
-
+mv
 # 3. List the contents of the ./data/raw directory
 
 # 4. Create the directory ./data/processed, 
