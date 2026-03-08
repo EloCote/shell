67f6e493762c8a4bd7d9b078f69e815fe69337a1
EloCote <eloise.cote@mail.utoronto.ca>
2026-03-08 17:06:16 -0400

    assignment questions completed

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -31,7 +31,7 @@ unzip -q rawdata.zip
 mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
-mv ./rawdata directory ./data/raw
+mv ./rawdata ./data/raw
 
 # 3. List the contents of the ./data/raw directory
 
@@ -56,8 +56,8 @@ cp  ./data/raw/*event*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
-rm *ipaddr*/./data/raw
-rm *ipaddr*/./data/processed/user_logs
+rm ./data/raw/*ipaddr*
+rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
