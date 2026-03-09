ac9820d0bd6961805d4cd3be25dca8b562fb7f15
EloCote <eloise.cote@mail.utoronto.ca>
2026-03-09 11:34:10 -0400

    addressed issue when merging

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -56,7 +56,6 @@ cp  ./data/raw/*user*.log ./data/processed/user_logs
 cp  ./data/raw/*event*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rf -rf ./data
 
 rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
@@ -69,3 +68,4 @@ ls ./data/processed/* > ./data/inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
+
